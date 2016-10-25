113c1e23d713ce7693034ce353d14ddc2ed037fb
DaafDP <david.el.depue@gmail.com>
2016-10-25 13:54:43 +0200

    subselection of representative Farms

---
+++ FADNselectionRepresentativeFarms.R
@@ -0,0 +1,11 @@
+#Reading in data
+BEL2012 <- read.csv("S:/team_jb/staff/ddpue/FADN/BEL2012.csv")
+Permits <- read.delim("C:/Users/ddpue/Documents/GPBV Flanders/R/GPBVFlanders/EnvironmentalPermits.txt")
+
+#Selection based on Type of Farm (TF). Specialist pigs, specialist poultry and mixed livestock
+TFs <- c(511, 512, 513, 521, 522, 523, 530, 732, 741)
+Subselection <- subset(BEL2012, BEL2012$A34 %in% TFs)
+
+#Only Flanders
+Subselection <- subset(Subselection, Subselection$A1 == 341)
+                   
